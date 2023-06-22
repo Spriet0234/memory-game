27a826b08bfc41876092d9649c15f104abdaa394
Santiago Prieto <sanpriet@publicisgroupe.net>
2023-06-22 16:45:32 -0400

    some changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Switch,
+  Route,
+  Link,
+  BrowserRouter,
+  NavLink,
+} from "react-router-dom";
+import StartPage from "./components/StartPage";
 import Card from "./components/card";
 import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <GameBoard deck={[1, 2, 3, 4, 5, 6, 7, 8]} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        ></a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <nav>
+            <h1>links</h1>
+            <Link to="/">Home</Link>
+            <NavLink to="/game">Game</NavLink>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<StartPage />} />
+          <Route path="/game" element={<GameBoard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/StartPage.js
@@ -0,0 +1,3 @@
+export default function StartPage() {
+  return <div>he</div>;
+}
